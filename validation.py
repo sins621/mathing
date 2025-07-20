f67828a1db871653f940ef15cc40f8355ee4d161
sins <sinsmailza@gmail.com>
2025-07-20 12:18:33 +0200

    feat: add validation functions to ensure user input is a valid number

---
+++ validation.py
@@ -0,0 +1,12 @@
+def choice_is_number(value: any) -> bool:
+    try:
+        value = int(value)
+        return True
+    except:
+        return False
+
+
+def validate_number(value) -> int:
+    while not choice_is_number(value):
+        value = input("Sorry, you didn't input a valid option, try again.\n\n")
+    return int(value)
