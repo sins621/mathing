1241612f5785542e4d4d470659f7a388ea0d844d
sins <sinsmailza@gmail.com>
2025-07-20 12:19:00 +0200

    feat: implement CLI for math practice selection and duration input

---
+++ cli.py
@@ -0,0 +1,31 @@
+from my_math import practice_table
+from my_time import practice_for_duration
+
+from validation import validate_number
+
+choice = validate_number(
+    input(
+        "What would you like to practice?\n1: Times Tables\n2: Addition & Subtraction\n\n"
+    )
+)
+
+match choice:
+    case 1:
+        duration = validate_number(
+            input("How long would you like to practice for?\n\n")
+        )
+        min_val = validate_number(
+            input("What's the minimum number for the tables?\n\n")
+        )
+        max_val = validate_number(
+            input("What's the maximum number for the tables?\n\n")
+        )
+
+        practice_function = lambda: practice_table(min_val, max_val)
+        practice_for_duration(duration, practice_function)
+    case 2:
+        pass
+    case _:
+        pass
+
+print("See you later")
