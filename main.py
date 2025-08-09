b917159059f93f9a814a19ee61c36195981c3691
sins <sinsmailza@gmail.com>
2025-08-09 21:12:48 +0200

    feat: initialize and run practicing from main

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from cli import start_practice
+from db import PracticeRegime, PracticeSession, db
+
+
+def initialize_db():
+    db.connect()
+    db.create_tables([PracticeSession, PracticeRegime], safe=True)
+
+
 def main():
-    print("Hello from mathing!")
+    initialize_db()
+    start_practice()
+    db.close()
+
 
 if __name__ == "__main__":
     main()
