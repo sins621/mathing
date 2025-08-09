47d98a78e147b69ccf2df6ef82c2d4ab321787f1
sins <sinsmailza@gmail.com>
2025-08-09 21:12:05 +0200

    update to return tuple of questions asked and correct answers

--- my_time.py
+++ my_time.py
@@ -4,8 +4,12 @@ import time
 def practice_for_duration(duration_minutes: int, exercise):
     duration_seconds = duration_minutes * 60
     start_time = time.time()
+    questions_asked = 0
+    correct_answers = 0
 
     while time.time() - start_time < duration_seconds:
-        exercise()
+        if exercise():
+            correct_answers += 1
+        questions_asked += 1
 
-    print("Time's up!")
+    return questions_asked, correct_answers
